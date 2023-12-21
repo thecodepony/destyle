5390512d3fdf9af16bf0d7e7ea2df4546d62a04a
thecodepony <codepony@proton.me>
2023-12-22 00:45:05 +0900

    remove spaces

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
@@ -18,15 +17,12 @@ fn main() -> eframe::Result<()> {
         Box::new(|cc| Box::new(destyle::TemplateApp::new(cc))),
     )
 }
-
 // When compiling to web using trunk:
 #[cfg(target_arch = "wasm32")]
 fn main() {
     // Redirect `log` message to `console.log` and friends:
     eframe::WebLogger::init(log::LevelFilter::Debug).ok();
-
     let web_options = eframe::WebOptions::default();
-
     wasm_bindgen_futures::spawn_local(async {
         eframe::WebRunner::new()
             .start(
