e903b59cb1d99d40e472117c5e9b5399ab0ef475
thecodepony <codepony@proton.me>
2023-12-22 10:05:26 +0900

    fix comment

--- src/app.rs
+++ src/app.rs
@@ -21,6 +21,7 @@ impl TemplateApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+        //
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
